87401a53e1ada94f8f58149c3f5f98d71dcb85fd
Nayeoonn <107173330+Nayeoonn@users.noreply.github.com>
2025-06-18 03:14:57 +0900

    feat: BlockedExtensionRepository 및 FixedExtensionRepository 구현

--- fileblocker/src/main/java/com/nayeon/fileblocker/repository/BlockedExtensionRepository.java
+++ fileblocker/src/main/java/com/nayeon/fileblocker/repository/BlockedExtensionRepository.java
@@ -1,5 +1,20 @@
 package com.nayeon.fileblocker.repository;
 
-public interface BlockedExtensionRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nayeon.fileblocker.domain.BlockedExtension;
+
+
+// 커스텀 확장자 저장/조희/중복 검사용
+public interface BlockedExtensionRepository extends JpaRepository<BlockedExtension, Long> {  
+                                            // BlockExtension이라는 엔티티를 기본키 타입인 Long인 기준으로 DB에 자동으로 연결해주는 인터페이스
     
+    // 동일한 확장자가 이미 등록되어 있는지 확인
+    boolean existsByExtension(String extension);
+
+    // 확장자 기준 조회(필요시)
+    Optional<BlockedExtension> findByExtension(String extension);
+    // Optional<T> : T객체가 있을수도 있고 없을 수도 있다는 것을 명시적으로 표현
 }
--- fileblocker/src/main/java/com/nayeon/fileblocker/repository/FixedExtensionRepository.java
+++ fileblocker/src/main/java/com/nayeon/fileblocker/repository/FixedExtensionRepository.java
@@ -1,5 +1,15 @@
 package com.nayeon.fileblocker.repository;
 
-public interface FixedExtensionRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nayeon.fileblocker.domain.FixedExtension;
+
+// 시스템에서 기본으로 제공하는 확장자의 상태를 저장/조회
+public interface FixedExtensionRepository extends JpaRepository<FixedExtension, Long>{
     
+    // 확장자 중복체크 (초기 설정 or 체크박스 상태 업데이트시 사용)
+    // 해당 확장자가 존재하면 Optional로 감싸진 엔티티 반환
+    Optional<FixedExtension> findByExtension(String extension);
 }

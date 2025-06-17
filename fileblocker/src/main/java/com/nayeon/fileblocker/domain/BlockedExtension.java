2ce2d18c1974b5403330f5d7a0f2b4e25799ba26
Nayeoonn <107173330+Nayeoonn@users.noreply.github.com>
2025-06-18 02:18:10 +0900

    feat: BlockedExtension 및 FixedExtension 엔티티 생성

--- fileblocker/src/main/java/com/nayeon/fileblocker/domain/BlockedExtension.java
+++ fileblocker/src/main/java/com/nayeon/fileblocker/domain/BlockedExtension.java
@@ -1,5 +1,36 @@
 package com.nayeon.fileblocker.domain;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity // JPA가 이 클래스를 DB 테이블로 인식
+@Table(name = "blocked_extension")
+@Getter
+@NoArgsConstructor      // 파라미터가 없는 기본 생성자를 자동으로 만들어주는 Lombok 어노테이션
 public class BlockedExtension {
     
+    // @Id + @GeneratedValue -> 기본키 자동 생성
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // autoinvrement
+    private Long id;        // -> 기본키
+
+    // 문자열 길이 varchar(20), null 허용 X, 증복금지
+    @Column(length = 20, nullable = false, unique = true)
+    private String extension;
+
+    private LocalDateTime createdAt;
+
+    public BlockedExtension(String extension){
+        this.extension = (extension.toLowerCase());    // 중복 방지를 위해 소문자로 통일
+        this.createdAt = LocalDateTime.now();
+    }
+
 }
--- fileblocker/src/main/java/com/nayeon/fileblocker/domain/FixedExtension.java
+++ fileblocker/src/main/java/com/nayeon/fileblocker/domain/FixedExtension.java
@@ -1,5 +1,40 @@
 package com.nayeon.fileblocker.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "fixed_extension")
+@Getter
+@NoArgsConstructor
 public class FixedExtension {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false, unique = true)
+    private String extension;
+
+    // 차단 여부 true  : 차단된 상태 (checkbox checked)
+    //         false : 허용된 상태
+    @Column(nullable = false)
+    private boolean checked;
+
+    // 확장자명과 차단 여부를 전달받기 위한 생성자
+    public FixedExtension(String extension, boolean checked) {
+        this.extension = extension.toLowerCase(); 
+        this.checked = checked;
+    }
+
+    // 외부에서 상태만 바꿀 수 있도록 허용
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
 }

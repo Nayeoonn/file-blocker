eed82a6d2f736816f76467ceb2f173388a3e8e19
Nayeoonn <107173330+Nayeoonn@users.noreply.github.com>
2025-06-17 18:06:19 +0900

    chore: 프로젝트 기본 구조 세팅

---
+++ fileblocker/src/main/java/com/nayeon/fileblocker/FileblockerApplication.java
@@ -0,0 +1,13 @@
+package com.nayeon.fileblocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileblockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileblockerApplication.class, args);
+	}
+
+}
---
+++ fileblocker/src/test/java/com/nayeon/fileblocker/FileblockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayeon.fileblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileblockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
